dd3935fdc371d317bdf26eca755c47b7f7443fdc
omarcruz <delacruz.sd@gmail.com>
2015-08-08 02:33:31 -0500

    Correcciones

--- app/src/main/java/senati/senatipeamobile/actLogin.java
+++ app/src/main/java/senati/senatipeamobile/actLogin.java
@@ -10,9 +10,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class actLogin extends AppCompatActivity implements View.OnClickListener {
     EditText txtUsuario, txtContraseña;
     Button btnIngresar;
+
+    private static String URL_LOGIN_USUARIO = "https://dam-atlas-asterope1.c9.io/app_usuarios/login?";
+    private static String USUARIO = "pStrLogin=";
+    private static String CONTRASEÑA = "pStrPwd=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,15 +66,100 @@ public class actLogin extends AppCompatActivity implements View.OnClickListener 
         String strContraseña = txtContraseña.getText().toString();
         if ( true ) {
             Bundle bundle = new Bundle();
-            bundle.putStringArray("Instructor", new String[]{"1", "Espinoza", "Manrique", "Omar", "Ali"});
-            Intent intent = new Intent( this, actCalendario.class );
-            intent.putExtras(bundle);
-            v.getContext().startActivity(intent);
+
+            //bundle.putStringArray("Instructor", new String[]{"1", "Espinoza", "Manrique", "Omar", "Ali"});
+
+            LoginUsuario(URL_LOGIN_USUARIO + USUARIO + strUsuario + "&" + CONTRASEÑA + strContraseña);
+
+            //Intent intent = new Intent( this, actCalendario.class );
+            //intent.putExtras(bundle);
+            //v.getContext().startActivity(intent);
         }
     }
 
     private void VerMensaje(String strMensaje) {
         Toast.makeText( this, strMensaje, Toast.LENGTH_SHORT ).show();
     }
+
+    private void LoginUsuario(String searchLoginUsuario) {
+        System.out.println("URL = " + searchLoginUsuario);
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(
+                Request.Method.GET, searchLoginUsuario, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // the response is already constructed as a JSONObject!
+                try {
+                    //books.clear();
+                    int resultsCount = response.getInt("numFound");
+                    JSONArray resultsArray = response.getJSONArray("docs");
+
+                    System.out.println("Results: " + resultsCount);
+
+                    int limit = resultsCount > 10 ? 10 : resultsCount;
+
+                    for (int position = 0; position < limit; position++) {
+                        JSONObject result = resultsArray.getJSONObject(position);
+
+                        String login = result.getString("login");
+                        System.out.println("login = " + login);
+
+                        String name = result.getString("name");
+                        System.out.println("name = " + name);
+
+                        String supervisor_id = result.getString("supervisor_id");
+                        System.out.println("supervisor_id = " + name);
+
+                        //String key = result.getString("key");
+                        //Long coverId = result.optLong("cover_i");
+                        //System.out.println("cover_i = " + coverId);
+                        //Boolean hasFullText = result.getBoolean("has_fulltext");
+                        //int firstPublishYear = result.optInt("first_publish_year");
+                        //int editionCount = result.getInt("edition_count");
+
+                        //JSONArray authorNames = result.optJSONArray("author_name");
+                        //JSONArray authorKeys = result.optJSONArray("author_key");
+                        //int authorsCount = authorNames == null ? 0 : authorNames.length();
+                        //ArrayList<Author> authors = new ArrayList<>();
+                        //for (int authorPosition = 0; authorPosition < authorsCount; authorPosition++) {
+                        //Author author = new Author(authorKeys.getString(authorPosition), authorNames.getString(authorPosition));
+                        //authors.add(author);
+                        //System.out.println("Author name = " + author.getName());
+                        //}
+
+                        //Cover cover = new Cover(
+                        //COVERS_SEARCH_URL + coverId + "-S.jpg",
+                        //COVERS_SEARCH_URL + coverId + "-M.jpg",
+                        //COVERS_SEARCH_URL + coverId + "-L.jpg"
+                        //);
+
+                        //Book book = new Book(key, coverId, hasFullText, title, authors, cover, firstPublishYear, editionCount);
+                        //books.add(book);
+                        //System.out.println("Book title = " + book.getTitle());
+                    }
+
+                    //System.out.println("searchTitles: Array Size " + Integer.toString(books.size()));
+
+                    //if (books.size() > 0) {
+                     //   Intent intent = new Intent(actLogin.this, BookCatalogActivity.class);
+                     //   startActivity(intent);
+                    //}
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }
+        );
+        Volley.newRequestQueue(this).add(jsonRequest);
+    }
+
+
 
 }
